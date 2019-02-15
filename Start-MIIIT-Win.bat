4864af6ffe72d22903cb7ea3cc1f177e83bb814b
Soren <soerendip@gmail.com>
2019-02-15 14:14:05 -0800

    Added Windows start batch file

---
+++ Start-MIIIT-Win.bat
@@ -0,0 +1,11 @@
+
+if exist C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate (
+    call  C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate miiit
+) else (
+    echo Could not find anaconda's script 'activate'.
+    echo Please, make sure it is in the PATH.
+    call activate miiit
+)
+
+start "" http://localhost:8888/apps/Metabolomics_Interactive_Intensity_Integration_Tool.ipynb?appmode_scroll=0
+jupyter notebook --no-browser &
