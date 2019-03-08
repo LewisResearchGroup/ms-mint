f12cca6c3c5f3ab932f2b309cfb8fcd1e1bb11f1
Soren Wacker <soerendip@gmail.com>
2019-03-08 15:16:21 -0700

    Windows

--- Start-MIIIT-Win.bat
+++ Start-MIIIT-Win.bat
@@ -1,6 +1,8 @@
 
 if exist C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate (
     call  C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate miiit
+) else if exist C:\Users\%USERNAME%\Miniconda3\Scripts\activate (
+    call C:\Users\%USERNAME%\Miniconda3\Scripts\activate miiit
 ) else (
     echo Could not find anaconda's script 'activate'.
     echo Please, make sure it is in the PATH.
