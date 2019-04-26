85cf6d136313b443da904a07f5d07716b44c25d4
Soren Wacker <soerendip@gmail.com>
2019-04-25 18:57:44 -0600

    start script for win

---
+++ scripts/Mint__windows.bat
@@ -0,0 +1,12 @@
+
+if exist C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate (
+    call  C:\Users\%USERNAME%\AppData\Local\Continuum\miniconda3\Scripts\activate mint
+) else if exist C:\Users\%USERNAME%\Miniconda3\Scripts\activate (
+    call C:\Users\%USERNAME%\Miniconda3\Scripts\activate mint
+) else (
+    echo Could not find anaconda's script 'activate'.
+    echo Please, make sure it is in the PATH.
+    call activate mint
+)
+
+Mint
