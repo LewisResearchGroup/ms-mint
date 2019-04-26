65e6ce0dc0d6b320139af82f50d0ee175f409650
Soren Wacker <soerendip@gmail.com>
2019-04-25 18:57:44 -0600

    start script for win

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'url': 'https://github.com/soerendip/mint',
     'author_email': 'swacker@ucalgary.ca',
     'version': 0.1,
-    'scripts': ['scripts/Mint'],
+    'scripts': ['scripts/Mint', 'scripts/Mint__windows.bat'],
     'packages': ['mint'],
     'name': 'mint',
     'data_files': [('static', ['static/Standard_Peaklist.csv']),
