e1e6ad4ce373d059637209d024765d1fa77fd5fb
Soren Wacker <soerendip@gmail.com>
2019-04-25 18:54:07 -0600

    path for windows corrected

--- scripts/Start-MIIIT-Linux.sh
+++
@@ -1,7 +0,0 @@
-PORT=9831
-source /home/`whoami`/.bashrc
-source activate miiit
-cd /home/swacker/workspace/uofc/code/metabolomics/MIIIT
-xdg-open "http://localhost:${PORT}/apps/notebooks/Metabolomics_Interactive_Intensity_Integration_Tool.ipynb?appmode_scroll=0"
-jupyter notebook --no-browser --port=${PORT}
-
