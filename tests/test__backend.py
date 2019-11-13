12a7db3a6e8b77bb78e7764d42d4e0afbc93c7bd
Soren <soerendip@gmail.com>
2019-11-13 15:50:53 -0700

    tidy up

---
+++ tests/test__backend.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.metrics import r2_score
+
+from mint.backend import Mint, STANDARD_PEAKFILE
+
+
+app = Mint()
+app.peaklist = STANDARD_PEAKFILE
+
+class TestClass():
+    def test__add_experimental_data(self):
+        app.files = ['tests/data/test.mzXML']
+
+    def test__add_peaklist(self):
+        app.peaklist = ['tests/data/test_peaklist.csv']
+
+    def test__app_run(self):
+        app.run()
+
+    def test__correct_peakAreas(self):
+        df_test = pd.read_csv('tests/data/test_peaklist.csv', dtype={'peakLabel': str})
+        df = pd.merge(df_test, app.results, on='peakLabel', suffixes=('_real', '_calc'))
+        print(df)
+        R2 = r2_score(df.peakArea_real, df.peakArea_calc)
+        assert R2 > 0.999
