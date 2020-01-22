e2701f822f041a3a831ace0628c09916cc32ee68
Soren Wacker <soerendip@gmail.com>
2020-01-22 11:54:23 -0700

    fix

--- ms_mint/__init__.py
+++ ms_mint/__init__.py
@@ -2,10 +2,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-#from .Mint import Mint
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
-from .backend import Mint
+from .Mint import Mint
