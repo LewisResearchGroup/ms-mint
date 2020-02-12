9cdd92266d0c1a8c58a0b6d47bf47a4a7b49f892
Soren <soerendip@gmail.com>
2020-02-12 11:23:05 -0700

    ..

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
+    'name': 'ms-mint-soerendip'
+    'version': '0.0.25',
     'description': 'Metabolomics Integration Tool (Mint)',
     'author': 'Soren Wacker',
     'url': 'https://github.com/soerendip/ms-mint',
     'author_email': 'swacker@ucalgary.ca',
-    'version': '0.0.25',
     'scripts': ['scripts/Mint.py', 'scripts/Mint.bat'],
     'packages': ['ms_mint'],
-    'name': 'ms_mint',
     'data_files': [('scripts', ['scripts/Mint.py', 'scripts/Mint.bat']),
                    ('static', ['static/Standard_Peaklist.csv'])]
 }
