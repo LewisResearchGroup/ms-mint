ca9d218fefff4f41122151cf7a9799f344d992f5
Soren <soerendip@gmail.com>
2020-02-12 13:57:08 -0700

    ..

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_requires = [
-    'pandas',
+    'pandas>=1',
     'plotly',
     'lxml',
     'matplotlib',
@@ -44,7 +44,7 @@ config = {
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-   'python_requires': '>=3.5',
+   'python_requires': '>=3.7',
    'install_requires': install_requires
 }
 
