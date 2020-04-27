f9629d5b724e31b2be8ab158dc98b16de075076a
Soren <soerendip@gmail.com>
2020-04-27 12:36:34 -0600

    0.0.30

--- ms_mint/__init__.py
+++ ms_mint/__init__.py
@@ -2,9 +2,3 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from .Mint import Mint
-from .tools import generate_grid_peaklist, integrate_peaks_from_filename
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
