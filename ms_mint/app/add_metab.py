f2c76329cce59ae685be188ad573278089976ef5
Soren Wacker <swacker@ucalgary.ca>
2021-01-21 11:35:21 -0700

    Add metabolites tab

---
+++ ms_mint/app/add_metab.py
@@ -0,0 +1,110 @@
+import os
+import pandas as pd
+
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
+from dash.exceptions import PreventUpdate
+from dash_tabulator import DashTabulator
+
+from ms_mint import MINT_DATA_PATH
+from . import tools as T
+
+M_PROTON = 1.00782503223
+
+options = {
+           "selectable": True,
+           "headerFilterLiveFilterDelay":3000,
+           "layout": "fitDataFill",
+           "height": "600px",
+           }
+
+clearFilterButtonType = {"css": "btn btn-outline-dark", "text":"Clear Filters"}
+
+fn_data = os.path.join(MINT_DATA_PATH, 'ChEBI.tsv')
+chcbi_data = pd.read_csv(fn_data, sep='\t', nrows=None, index_col=0)
+chcbi_data = chcbi_data[chcbi_data['Charge'].isin([-1,0,1])]
+chcbi_data = chcbi_data[chcbi_data['Monoisotopic Mass'].notna()]
+chcbi_data = chcbi_data[chcbi_data['Monoisotopic Mass']<1000]
+chcbi_data = chcbi_data[chcbi_data['Monoisotopic Mass']>50]
+chcbi_data['Monoisotopic Mass'] = chcbi_data['Monoisotopic Mass'].astype(float)
+
+#print('Available columns:')
+#for col in chcbi_data: print(col)
+
+chcbi_data = chcbi_data[['ChEBI ID', 'ChEBI Name', 'Formulae', 'Charge', 'Monoisotopic Mass', 'Synonyms', 'SMILES', 'Definition']]
+
+
+columns = T.gen_tabulator_columns(chcbi_data.columns, add_color_col=False, add_ms_file_col=False, editor=None)
+
+
+add_metab_table = html.Div(id='add-metab-table-container', 
+    style={'min-height':  100, 'margin-top': '10%'},
+    children=[
+        dcc.Dropdown(id='add-metab-ms-mode', placeholder='Select ionization mode',
+            options=[{'label': 'Positive', 'value': 'Positive'},
+                     {'label': 'Negative', 'value': 'Negative'}], 
+            value='Negative'),
+        DashTabulator(id='add-metab-table',
+            columns=columns, 
+            options=options,
+            clearFilterButtonType=clearFilterButtonType,
+            data=chcbi_data.to_dict('records')
+        ),
+    html.Button('Add selected metabolites to peaklist', id='add-metab'),
+    html.Div(id='add-metab-output'),
+
+])
+
+
+_layout = html.Div([
+    html.H3('Add Metabolites'),
+    add_metab_table
+    
+])
+
+def layout():
+    return _layout
+
+
+def callbacks(app, fsc, cache):
+
+    @app.callback(
+        Output('add-metab-output', 'children'),
+        Input( 'add-metab', 'n_clicks'),
+        State( 'add-metab-table', 'multiRowsClicked'),
+        State( 'add-metab-ms-mode', 'value'),
+        State( 'wdir', 'children')
+    )
+    def add_metab(n_clicks, rows, ms_mode, wdir):
+        if n_clicks is None:
+            raise PreventUpdate
+        if ms_mode is None:
+            return 'Please select ionization mode.'
+        peaklist = T.get_peaklist( wdir )
+        for row in rows:
+            charge = int( row['Charge'] )
+            if (ms_mode=='Negative') and (charge==1):
+                continue
+            if (ms_mode=='Positive') and (charge==-1):
+                continue
+            print(charge)
+            if charge == 0:
+                if ms_mode == 'Negative':         
+                    peaklist.loc[row['ChEBI Name'], 'mz_mean'] = row['Monoisotopic Mass'] - M_PROTON
+                elif ms_mode == 'Positive':
+                    peaklist.loc[row['ChEBI Name'], 'mz_mean'] = row['Monoisotopic Mass'] + M_PROTON
+            else:
+                peaklist.loc[row['ChEBI Name'], 'mz_mean'] = row['Monoisotopic Mass']
+
+            peaklist.loc[row['ChEBI Name'], 'mz_width'] = 10
+            peaklist.loc[row['ChEBI Name'], 'rt'] = -1
+            peaklist.loc[row['ChEBI Name'], 'rt_min'] = 0
+            peaklist.loc[row['ChEBI Name'], 'rt_max'] = 100
+
+        peaklist.to_csv( T.get_peaklist_fn( wdir ))
+        print(peaklist)
+        return 'Metabolites added.'
+
+        
