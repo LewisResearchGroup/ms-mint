28ab3d4a329bf9c41b5c1d13cc56d04fab392469
Soren Wacker <soerendip@gmail.com>
2021-09-22 13:27:05 -0600

    Docker for MINT server

--- flaskapp/serve.py
+++ flaskapp/serve.py
@@ -2,5 +2,5 @@ from waitress import serve
 
 from flaskapp import create_app
 
-serve(create_app, host='127.0.0.1', port=8000, url_scheme='http')
+serve(create_app, host='127.0.0.1', port=8080, url_scheme='http')
 
