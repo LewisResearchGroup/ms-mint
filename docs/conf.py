a9adf9f931b680892a64b7383d8885a94a004106
Soren Wacker <soerendip@gmail.com>
2022-05-25 17:24:19 -0600

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,8 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -65,3 +66,5 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 source_suffix = [".rst"]
+
+html_baseurl = 'https://lewisresearchgroup.github.io/ms-mint'
