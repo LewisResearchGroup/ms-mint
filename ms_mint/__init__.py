a8ed375b9449e5794dd44891096cea3700ad57c1
GitHub Action <action@github.com>
2022-05-25 23:17:12 +0000

    Add changes for 1e8771b0561a061baf95e80c6bf07985b3acf0d1

--- ms_mint/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from ._version import get_versions
-from .Mint import Mint
-
-__version__ = get_versions()["version"]
-del get_versions
-
-MINT_DATA_PATH = os.path.abspath(os.path.join(__path__[0], "..", "static"))
--- tests/test__pca.py
+++
@@ -1,7 +0,0 @@
-from ms_mint.Mint import Mint
-
-
-def test__run_pca():
-    mint = Mint()
-    mint.load("tests/data/results/example_results.csv")
-    mint.pca()
