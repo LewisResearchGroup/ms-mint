f2998d73db8a904a5e8c65d0bb3e8afa6d0519a7
Soren Wacker <soerendip@gmail.com>
2022-05-26 13:20:40 -0600

    relocated plotting tools

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,10 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-#import sys
+import sys
 import sphinx_rtd_theme
 
-#sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
