5cd20e21393c6c1a8410c49f994412fa01eeddfd
Soren Wacker <soerendip@gmail.com>
2022-06-09 11:55:12 -0600

    Do something with import...

--- ms_mint/__init__.py
+++ ms_mint/__init__.py
@@ -1,6 +1,7 @@
 "A Python library for targetd metabolomics."
 
 import os
+import logging
 from ._version import get_versions
 from .Mint import Mint
 
@@ -8,3 +9,5 @@ __version__ = get_versions()["version"]
 del get_versions
 
 MINT_DATA_PATH = os.path.abspath(os.path.join(__path__[0], "..", "static"))
+
+logging.info(Mint.version)
