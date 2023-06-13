e587e729117046aada9b926688272b9772c9e8b9
Soren Wacker <swacker@ucalgary.ca>
2023-06-13 12:40:56 -0600

    sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -89,9 +89,9 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 #html_theme = "alabaster"
-html_theme = 'sphinx_materialdesign_theme'
+#html_theme = 'sphinx_materialdesign_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
