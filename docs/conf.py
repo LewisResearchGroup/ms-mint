cb52f6b10cdee1279b2c5e7866dfbd0c46f1c144
Soren Wacker <swacker@ucalgary.ca>
2023-06-13 12:56:10 -0600

    gtag

--- docs/conf.py
+++ docs/conf.py
@@ -89,12 +89,13 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 
-html_theme = "alabaster"
+#html_theme = "alabaster"
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 source_suffix = [".rst"]
 
@@ -103,7 +104,3 @@ html_baseurl = "https://lewisresearchgroup.github.io/ms-mint"
 requirements_path = "docs/requirements.txt"
 
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-  'analytics_id': 'KZQBC4NMD2'
-}
