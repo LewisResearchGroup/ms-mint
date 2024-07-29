7b60a83c30df2714266696cf3f413baf341744d8
Soren Wacker <s.wacker@tudelft.nl>
2024-07-29 11:42:05 +0200

    new repository file structure for toml based installation

--- src/ms_mint/__init__.py
+++ src/ms_mint/__init__.py
@@ -1,14 +1,13 @@
 import os
 import logging
 from .Mint import Mint
+from ._version import __version__
 
-try:
-    from .version import __version__
-except ImportError:
-    __version__ = "0.0.0"
+__all__ = ["__version__"]
 
 MINT_DATA_PATH = os.path.abspath(os.path.join(__path__[0], "..", "static"))
 
 Mint.version = __version__
 
 logging.info(Mint.version)
+
